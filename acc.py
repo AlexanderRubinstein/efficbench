a8454f0acf3d413a619b9b39021e1f8241789ff2
Alexander Rubinstein <rubalex14@gmail.com>
2025-05-04 22:23:33 +0200

    - Properly sort models wrt date - Extend mlps zoo

--- acc.py
+++ acc.py
@@ -33,7 +33,15 @@ BASE_ESTIMATORS = [
     "KNN", # [ADD][new estimator]
 ]
 
-BEST_MLP_FITTING_METHODS = []
+BEST_MLP_FITTING_METHODS = [
+    ('MLP3_e600_lr0.001', (
+        MLPRegressor, {
+            'hidden_channels': [128, 128, 1],
+            'n_epochs': 600,
+            'lr': 0.001,
+        }
+    )),
+]
 MLP_FITTING_METHODS = [
     # MLP 2 layers
     ('MLP2_e50_lr0.001', (
@@ -128,10 +136,31 @@ MLP_FITTING_METHODS = [
             'lr': 0.0001,
         }
     )),
-    ('MLP3_e600_lr0.001', (
+    # ('MLP3_e600_lr0.001', (
+    #     MLPRegressor, {
+    #         'hidden_channels': [128, 128, 1],
+    #         'n_epochs': 600,
+    #         'lr': 0.001,
+    #     }
+    # )),
+    ('MLP3_e600_lr0.0001', (
         MLPRegressor, {
             'hidden_channels': [128, 128, 1],
             'n_epochs': 600,
+            'lr': 0.0001,
+        }
+    )),
+    ('MLP3_e700_lr0.001', (
+        MLPRegressor, {
+            'hidden_channels': [128, 128, 1],
+            'n_epochs': 700,
+            'lr': 0.001,
+        }
+    )),
+    ('MLP3_e800_lr0.001', (
+        MLPRegressor, {
+            'hidden_channels': [128, 128, 1],
+            'n_epochs': 800,
             'lr': 0.001,
         }
     )),
