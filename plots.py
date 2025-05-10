41c68d2be362297f3765f1e6b4702859138969b4
Alexander Rubinstein <rubalex14@gmail.com>
2025-05-10 21:44:55 +0200

    Use make_table_avg in generate_table_avg

--- plots.py
+++ plots.py
@@ -324,3 +324,149 @@ def load_scores(bench, split, scenarios_to_skip=[], ordered=False):
     scores = np.vstack([scores[:,scenarios_position[scenario]].mean(axis=1) for scenario in scenarios])
 
     return scores[:,list(data.keys())]
+
+
+def make_table_avg(
+    bench,
+    split,
+    filename_suffix,
+    accs_full,
+    scenarios_to_skip,
+    return_perf_table=False,
+    ordered=True,
+    results='acc'
+):
+    table_avg = {}
+    table_std = {}
+    model_perf = {} # not used?
+    # for bench in benchs:
+    table_avg[bench] = {}
+    table_std[bench] = {}
+    model_perf[bench] = {}
+
+    agg = 'leaderboard' # 'leaderboard', 'scenarios'
+    # results = 'acc'# 'acc', 'rank'
+
+    if results == 'acc': ylim = (0,.1)
+    elif results == 'rank':
+        if agg_metric == 'std': ylim = (0,.1)
+        else: ylim = (.5,1)
+    else: raise NotImplementedError
+
+    # for split in splits[bench]:
+    table_avg[bench][split] = {}
+    table_std[bench][split] = {}
+    model_perf[bench][split] = {}
+
+    if bench == 'mmlu_fields' and split == 'iid':
+        filename_suffix = filename_suffix
+    else:
+        filename_suffix = ''
+
+    # full_results_path = f'results/accs_{bench}_split-{split}_iterations-5{filename_suffix}.pickle'
+
+    # with open(full_results_path, 'rb') as handle:
+    #     data = pickle.load(handle)
+
+    data = accs_full
+
+    models = list(data.keys())
+    number_items = list(data[models[0]].keys())
+    methods = list(data[models[0]][number_items[0]].keys())
+    scenarios = list(data[models[0]][number_items[0]][methods[0]].keys())
+
+    data = np.array([[[[data[model][number_item][method][scenario] for scenario in scenarios]  for model in data.keys()] for number_item in number_items] for method in methods])
+    scores = load_scores(bench, split, scenarios_to_skip=scenarios_to_skip, ordered=ordered)
+
+    if agg == 'leaderboard':
+        if bench=='helm':
+            ###
+            if results == 'acc':
+                ###
+                model_perf[bench][split]['truth'] = winrate(scores, axis=1).mean(axis=0)
+                for i,method in enumerate(methods):
+                    model_perf[bench][split][method] = {}
+                    model_perf[bench][split][method] = {}
+                for j,number_item in enumerate(number_items):
+                    model_perf[bench][split][method][number_item] = winrate(data, axis=2).mean(axis=3)[i,j,:,:]
+                ###
+                data = np.abs(winrate(data, axis=2).mean(axis=3)-winrate(scores, axis=1).mean(axis=0)[None,None,:,None])
+            elif results == 'rank':
+                rank_corrs = np.zeros(data.mean(axis=2).mean(axis=2).shape)
+                #print(bench,rank_corrs.shape)
+                for i in range(rank_corrs.shape[0]):
+                    for j in range(rank_corrs.shape[1]):
+                        for l in range(rank_corrs.shape[2]):
+                            #print(winrate(data, axis=2).mean(axis=3).shape)
+                            rank_corrs[i,j,l] = stats.spearmanr(winrate(data, axis=2).mean(axis=3)[i,j,:,l], winrate(scores.T, axis=0).mean(axis=1)).statistic
+                data=rank_corrs
+
+            else:
+                raise NotImplementedError
+        else:
+            ###
+            if results == 'acc':
+
+                # ###
+                model_perf[bench][split]['truth'] = scores.mean(axis=0)
+                for i,method in enumerate(methods):
+                    model_perf[bench][split][method] = {}
+                    model_perf[bench][split][method] = {}
+                    for j,number_item in enumerate(number_items):
+                        model_perf[bench][split][method][number_item] = data.mean(axis=3)[i,j,:,:]
+                # ###
+
+                data = np.abs(data.mean(axis=3)-scores.mean(axis=0)[None,None,:,None])
+            elif results == 'rank':
+                rank_corrs = np.zeros(data.mean(axis=2).mean(axis=2).shape)
+                #print(bench,rank_corrs.shape)
+                for i in range(rank_corrs.shape[0]):
+                    for j in range(rank_corrs.shape[1]):
+                        for l in range(rank_corrs.shape[2]):
+                            #print(data.mean(axis=3).shape)
+                            rank_corrs[i,j,l] = stats.spearmanr(data.mean(axis=3)[i,j,:,l], scores.T.mean(axis=1)).statistic
+                data=rank_corrs
+            else:
+                raise NotImplementedError
+    elif agg == 'scenarios':
+        if results == 'acc':
+            data = np.abs(data-scores.T[None,None,:,:,None]).mean(axis=3)
+        elif results == 'rank':
+            rank_corrs = np.zeros(data.mean(axis=2).shape)
+            for i in range(rank_corrs.shape[0]):
+                for j in range(rank_corrs.shape[1]):
+                    for k in range(rank_corrs.shape[2]):
+                        for l in range(rank_corrs.shape[3]):
+                            rank_corrs[i,j,k,l] = stats.spearmanr(data[i,j,:,k,l], scores.T[:,k]).statistic
+            data=rank_corrs
+        else:
+            raise NotImplementedError
+    else:
+        raise NotImplementedError
+
+    if agg_metric=='avg':
+        data = data.mean(-1) #iterations
+    elif agg_metric=='std':
+        data = data.std(-1)
+    else:
+        raise NotImplementedError
+
+
+    for i,method in enumerate(methods):
+        table_avg[bench][split][method] = {}
+        table_std[bench][split][method] = {}
+
+        for j,number_item in enumerate(number_items):
+            if agg == 'leaderboard' and results == 'rank':
+                #print(data.shape)
+                table_avg[bench][split][method][number_item] = data[i,j]
+                table_std[bench][split][method][number_item] = 0
+            else:
+                #print(data.shape)
+                table_avg[bench][split][method][number_item] = np.mean(data, axis=-1)[i,j]
+                table_std[bench][split][method][number_item] = data.std(-1)[i,j]
+
+    res = [table_avg, table_std]
+    if return_perf_table:
+        res = res + [model_perf]
+    return res
--- scripts/generate_table_avg.py
+++ scripts/generate_table_avg.py
@@ -12,9 +12,12 @@ from plots import (
     methods,
     # number_items,
     agg_metric,
-    load_scores
+    load_scores,
+    make_table_avg
 )
-from utils import dump_pickle
+from utils import (
+    dump_pickle,
+)
 sys.path.pop(0)
 
 
@@ -22,141 +25,181 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--filename_suffix_mmlu_fields', type=str, help='path suffix', default='')
     parser.add_argument('--scenarios_to_skip', type=str, help='scenarios to skip', default='')
+    parser.add_argument('--ordered', action='store_true', help='ordered', default=False)
+    parser.add_argument('--agg_type', type=str, help='agg type', default='acc', choices=['acc', 'rank'])
     args = parser.parse_args()
     filename_suffix_mmlu_fields = args.filename_suffix_mmlu_fields
-
-    agg = 'leaderboard' # 'leaderboard', 'scenarios'
-    results = 'acc'# 'acc', 'rank'
-
-    if results == 'acc': ylim = (0,.1)
-    elif results == 'rank':
-        if agg_metric == 'std': ylim = (0,.1)
-        else: ylim = (.5,1)
-    else: raise NotImplementedError
 
     table_avg = {}
     table_std = {}
     model_perf = {}
+
     for bench in benchs:
+
         table_avg[bench] = {}
         table_std[bench] = {}
         model_perf[bench] = {}
-
         for split in splits[bench]:
             table_avg[bench][split] = {}
             table_std[bench][split] = {}
             model_perf[bench][split] = {}
-
             if bench == 'mmlu_fields' and split == 'iid':
                 filename_suffix = filename_suffix_mmlu_fields
             else:
                 filename_suffix = ''
 
             full_results_path = f'results/accs_{bench}_split-{split}_iterations-5{filename_suffix}.pickle'
-
             with open(full_results_path, 'rb') as handle:
                 data = pickle.load(handle)
+            ordered = args.ordered if bench == "mmlu_fields" else False
+            current_table_avg, current_table_std, current_model_perf = make_table_avg(
+                bench,
+                split,
+                filename_suffix,
+                data,
+                scenarios_to_skip=args.scenarios_to_skip.split(','),
+                ordered=ordered,
+                return_perf_table=True,
+                results=args.agg_type
+            )
+            # print("DEBUG: current_table_avg", current_table_avg[bench][split].keys())
+            table_avg[bench][split] = current_table_avg[bench][split]
+            table_std[bench][split] = current_table_std[bench][split]
+            model_perf[bench][split] = current_model_perf[bench][split]
 
-            models = list(data.keys())
-            number_items = list(data[models[0]].keys())
-            methods = list(data[models[0]][number_items[0]].keys())
-            scenarios = list(data[models[0]][number_items[0]][methods[0]].keys())
+    # ??
+    # agg = 'leaderboard' # 'leaderboard', 'scenarios'
+    # results = 'acc'# 'acc', 'rank'
 
-            # DEBUG: when can not create np array
-            # for method in methods:
-            #     for number_item in number_items:
-            #         for model in models:
-            #             for scenario in scenarios:
-            #                 print(model, number_item, method, scenario, len(data[model][number_item][method][scenario]))
+    # if results == 'acc': ylim = (0,.1)
+    # elif results == 'rank':
+    #     if agg_metric == 'std': ylim = (0,.1)
+    #     else: ylim = (.5,1)
+    # else: raise NotImplementedError
 
-            data = np.array([[[[data[model][number_item][method][scenario] for scenario in scenarios]  for model in data.keys()] for number_item in number_items] for method in methods])
-            scores = load_scores(bench, split, scenarios_to_skip=args.scenarios_to_skip.split(','))
+    # table_avg = {}
+    # table_std = {}
+    # model_perf = {}
+    # for bench in benchs:
+    #     table_avg[bench] = {}
+    #     table_std[bench] = {}
+    #     model_perf[bench] = {}
 
-            if agg == 'leaderboard':
-                if bench=='helm':
-                    ###
-                    if results == 'acc':
-                        ###
-                        model_perf[bench][split]['truth'] = winrate(scores, axis=1).mean(axis=0)
-                        for i,method in enumerate(methods):
-                            model_perf[bench][split][method] = {}
-                            model_perf[bench][split][method] = {}
-                        for j,number_item in enumerate(number_items):
-                            model_perf[bench][split][method][number_item] = winrate(data, axis=2).mean(axis=3)[i,j,:,:]
-                        ###
-                        data = np.abs(winrate(data, axis=2).mean(axis=3)-winrate(scores, axis=1).mean(axis=0)[None,None,:,None])
-                    elif results == 'rank':
-                        rank_corrs = np.zeros(data.mean(axis=2).mean(axis=2).shape)
-                        #print(bench,rank_corrs.shape)
-                        for i in range(rank_corrs.shape[0]):
-                            for j in range(rank_corrs.shape[1]):
-                                for l in range(rank_corrs.shape[2]):
-                                    #print(winrate(data, axis=2).mean(axis=3).shape)
-                                    rank_corrs[i,j,l] = stats.spearmanr(winrate(data, axis=2).mean(axis=3)[i,j,:,l], winrate(scores.T, axis=0).mean(axis=1)).statistic
-                        data=rank_corrs
+    #     for split in splits[bench]:
+    #         table_avg[bench][split] = {}
+    #         table_std[bench][split] = {}
+    #         model_perf[bench][split] = {}
 
-                    else:
-                        raise NotImplementedError
-                else:
-                    ###
-                    if results == 'acc':
-                        # ###
-                        model_perf[bench][split]['truth'] = scores.mean(axis=0)
-                        for i,method in enumerate(methods):
-                            model_perf[bench][split][method] = {}
-                            model_perf[bench][split][method] = {}
-                            for j,number_item in enumerate(number_items):
-                                model_perf[bench][split][method][number_item] = data.mean(axis=3)[i,j,:,:]
-                        # ###
-                        data = np.abs(data.mean(axis=3)-scores.mean(axis=0)[None,None,:,None])
-                    elif results == 'rank':
-                        rank_corrs = np.zeros(data.mean(axis=2).mean(axis=2).shape)
-                        #print(bench,rank_corrs.shape)
-                        for i in range(rank_corrs.shape[0]):
-                            for j in range(rank_corrs.shape[1]):
-                                for l in range(rank_corrs.shape[2]):
-                                    #print(data.mean(axis=3).shape)
-                                    rank_corrs[i,j,l] = stats.spearmanr(data.mean(axis=3)[i,j,:,l], scores.T.mean(axis=1)).statistic
-                        data=rank_corrs
-                    else:
-                        raise NotImplementedError
-            elif agg == 'scenarios':
-                if results == 'acc':
-                    data = np.abs(data-scores.T[None,None,:,:,None]).mean(axis=3)
-                elif results == 'rank':
-                    rank_corrs = np.zeros(data.mean(axis=2).shape)
-                    for i in range(rank_corrs.shape[0]):
-                        for j in range(rank_corrs.shape[1]):
-                            for k in range(rank_corrs.shape[2]):
-                                for l in range(rank_corrs.shape[3]):
-                                    rank_corrs[i,j,k,l] = stats.spearmanr(data[i,j,:,k,l], scores.T[:,k]).statistic
-                    data=rank_corrs
-                else:
-                    raise NotImplementedError
-            else:
-                raise NotImplementedError
+    #         if bench == 'mmlu_fields' and split == 'iid':
+    #             filename_suffix = filename_suffix_mmlu_fields
+    #         else:
+    #             filename_suffix = ''
 
-            if agg_metric=='avg':
-                data = data.mean(-1) #iterations
-            elif agg_metric=='std':
-                data = data.std(-1)
-            else:
-                raise NotImplementedError
+    #         full_results_path = f'results/accs_{bench}_split-{split}_iterations-5{filename_suffix}.pickle'
+
+    #         with open(full_results_path, 'rb') as handle:
+    #             data = pickle.load(handle)
+
+    #         models = list(data.keys())
+    #         number_items = list(data[models[0]].keys())
+    #         methods = list(data[models[0]][number_items[0]].keys())
+    #         scenarios = list(data[models[0]][number_items[0]][methods[0]].keys())
+
+    #         # DEBUG: when can not create np array
+    #         # for method in methods:
+    #         #     for number_item in number_items:
+    #         #         for model in models:
+    #         #             for scenario in scenarios:
+    #         #                 print(model, number_item, method, scenario, len(data[model][number_item][method][scenario]))
+
+    #         data = np.array([[[[data[model][number_item][method][scenario] for scenario in scenarios]  for model in data.keys()] for number_item in number_items] for method in methods])
+    #         scores = load_scores(bench, split, scenarios_to_skip=args.scenarios_to_skip.split(','))
+
+    #         if agg == 'leaderboard':
+    #             if bench=='helm':
+    #                 ###
+    #                 if results == 'acc':
+    #                     ###
+    #                     model_perf[bench][split]['truth'] = winrate(scores, axis=1).mean(axis=0)
+    #                     for i,method in enumerate(methods):
+    #                         model_perf[bench][split][method] = {}
+    #                         model_perf[bench][split][method] = {}
+    #                     for j,number_item in enumerate(number_items):
+    #                         model_perf[bench][split][method][number_item] = winrate(data, axis=2).mean(axis=3)[i,j,:,:]
+    #                     ###
+    #                     data = np.abs(winrate(data, axis=2).mean(axis=3)-winrate(scores, axis=1).mean(axis=0)[None,None,:,None])
+    #                 elif results == 'rank':
+    #                     rank_corrs = np.zeros(data.mean(axis=2).mean(axis=2).shape)
+    #                     #print(bench,rank_corrs.shape)
+    #                     for i in range(rank_corrs.shape[0]):
+    #                         for j in range(rank_corrs.shape[1]):
+    #                             for l in range(rank_corrs.shape[2]):
+    #                                 #print(winrate(data, axis=2).mean(axis=3).shape)
+    #                                 rank_corrs[i,j,l] = stats.spearmanr(winrate(data, axis=2).mean(axis=3)[i,j,:,l], winrate(scores.T, axis=0).mean(axis=1)).statistic
+    #                     data=rank_corrs
+
+    #                 else:
+    #                     raise NotImplementedError
+    #             else:
+    #                 ###
+    #                 if results == 'acc':
+    #                     # ###
+    #                     model_perf[bench][split]['truth'] = scores.mean(axis=0)
+    #                     for i,method in enumerate(methods):
+    #                         model_perf[bench][split][method] = {}
+    #                         model_perf[bench][split][method] = {}
+    #                         for j,number_item in enumerate(number_items):
+    #                             model_perf[bench][split][method][number_item] = data.mean(axis=3)[i,j,:,:]
+    #                     # ###
+    #                     data = np.abs(data.mean(axis=3)-scores.mean(axis=0)[None,None,:,None])
+    #                 elif results == 'rank':
+    #                     rank_corrs = np.zeros(data.mean(axis=2).mean(axis=2).shape)
+    #                     #print(bench,rank_corrs.shape)
+    #                     for i in range(rank_corrs.shape[0]):
+    #                         for j in range(rank_corrs.shape[1]):
+    #                             for l in range(rank_corrs.shape[2]):
+    #                                 #print(data.mean(axis=3).shape)
+    #                                 rank_corrs[i,j,l] = stats.spearmanr(data.mean(axis=3)[i,j,:,l], scores.T.mean(axis=1)).statistic
+    #                     data=rank_corrs
+    #                 else:
+    #                     raise NotImplementedError
+    #         elif agg == 'scenarios':
+    #             if results == 'acc':
+    #                 data = np.abs(data-scores.T[None,None,:,:,None]).mean(axis=3)
+    #             elif results == 'rank':
+    #                 rank_corrs = np.zeros(data.mean(axis=2).shape)
+    #                 for i in range(rank_corrs.shape[0]):
+    #                     for j in range(rank_corrs.shape[1]):
+    #                         for k in range(rank_corrs.shape[2]):
+    #                             for l in range(rank_corrs.shape[3]):
+    #                                 rank_corrs[i,j,k,l] = stats.spearmanr(data[i,j,:,k,l], scores.T[:,k]).statistic
+    #                 data=rank_corrs
+    #             else:
+    #                 raise NotImplementedError
+    #         else:
+    #             raise NotImplementedError
+
+    #         if agg_metric=='avg':
+    #             data = data.mean(-1) #iterations
+    #         elif agg_metric=='std':
+    #             data = data.std(-1)
+    #         else:
+    #             raise NotImplementedError
 
 
-            for i,method in enumerate(methods):
-                table_avg[bench][split][method] = {}
-                table_std[bench][split][method] = {}
+    #         for i,method in enumerate(methods):
+    #             table_avg[bench][split][method] = {}
+    #             table_std[bench][split][method] = {}
 
-                for j,number_item in enumerate(number_items):
-                    if agg == 'leaderboard' and results == 'rank':
-                        #print(data.shape)
-                        table_avg[bench][split][method][number_item] = data[i,j]
-                        table_std[bench][split][method][number_item] = 0
-                    else:
-                        #print(data.shape)
-                        table_avg[bench][split][method][number_item] = np.mean(data, axis=-1)[i,j]
-                        table_std[bench][split][method][number_item] = data.std(-1)[i,j]
+    #             for j,number_item in enumerate(number_items):
+    #                 if agg == 'leaderboard' and results == 'rank':
+    #                     #print(data.shape)
+    #                     table_avg[bench][split][method][number_item] = data[i,j]
+    #                     table_std[bench][split][method][number_item] = 0
+    #                 else:
+    #                     #print(data.shape)
+    #                     table_avg[bench][split][method][number_item] = np.mean(data, axis=-1)[i,j]
+    #                     table_std[bench][split][method][number_item] = data.std(-1)[i,j]
 
         # with open('results/table_avg.pickle', 'wb') as handle:
         #     pickle.dump(table_avg, handle)
